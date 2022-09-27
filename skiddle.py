f5e95ab819cf9fbb20fc5a985ad9bc2258762929
travis-scholtens <113060760+travis-scholtens@users.noreply.github.com>
2022-09-27 15:36:17 -0400

    Update skiddle.py

--- skiddle.py
+++ skiddle.py
@@ -1,6 +1,27 @@
 from community import community_louvain
+from dataclasses import dataclass 
 import itertools
 import networkx
+import os
 import trueskill
+from typing import *
 
-print('hi')
+@dataclass
+class Url:
+  url: str
+
+def bootstrap(url: Url):
+  pass
+
+def updated_skills():
+  pass
+
+def updated_pti():
+  pass
+
+if __name__ == '__main__':
+  if os.environ.get('BOOTSTRAP'):
+    bootstrap(Url(os.environ['HOME_URL']))
+  else:
+    update_skills()
+    update_pti()
