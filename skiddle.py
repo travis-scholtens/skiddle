9802dd5f71cc4ab352728690bf15625ff4a5de0e
travis-scholtens <113060760+travis-scholtens@users.noreply.github.com>
2022-10-13 13:14:03 -0400

    Update skiddle.py

--- skiddle.py
+++ skiddle.py
@@ -627,9 +627,9 @@ def get_pti(home: BeautifulSoup,
 def partner_skills(roster: list[Player],
                    partners: set[tuple[Player, Player]],
                    skill: dict[Player, ttt.Gaussian]
-                   ) -> list[tuple[Player, Player, float, float, float]]:
+                   ) -> list[tuple[str, str, float, float, float]]:
   stats = []
-  for (a, b) in itertools.prohuct(roster, roster):
+  for (a, b) in itertools.product(roster, roster):
     if a < b and (a, b) in partners and all([p in skill for p in (a, b)]):
       combo = skill[a] + skill[b]
       stats.append([a.name, b.name, combo.mu, combo.sigma, combo.mu - 3*combo.sigma/2])
